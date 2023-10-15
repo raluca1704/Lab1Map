8675426e50c9d1cc013bda593062ada4a787c156
raluc <raluca.chindris17@gmail.com>
2023-10-15 15:30:36 +0300

    testele bad case pt pb3 si 4

--- src/ElectronicShopTests.java
+++ src/ElectronicShopTests.java
@@ -1,49 +1,90 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class ElectronicShopTests {
     @Test
-    public void biligsteTastatur() {
+    public void biligsteTastaturTestGood() {
         int[] input = {40, 35, 70, 15, 45};
         int outputExpected = ElectronicShop.biligsteTastatur(input);
         int output = 15;
         assertEquals(output, outputExpected);
-        System.out.println("\nBiligsteTastatur Test Succes.");
+        System.out.println("\nBiligsteTastatur Test Good Succes.");
+
+    }
+    @Test
+    public void biligsteTastaturTestBad() {
+        int[] input = {40, 35, 70, 15, 45};
+        int outputExpected = ElectronicShop.biligsteTastatur(input);
+        int output = 1;
+        assertFalse((output==outputExpected), "for test to succes output should be wrong");
+        System.out.println("\nBiligsteTastatur Test Bad Succes.");
 
     }
 
     @Test
-    public void teurestGegenstand() {
+    public void teurestGegenstandTestGood() {
         int[] input1 = {15, 20, 10, 35};
         int[] input2 = {20, 15, 40, 15};
         int outputExpected = ElectronicShop.teurestGegenstand(input1, input2);
         int output = 40;
         assertEquals(output, outputExpected);
-        System.out.println("\nTeurestGegenstand Test Succes.");
+        System.out.println("\nTeurestGegenstand Test Good Succes.");
+
+    }
+    @Test
+    public void teurestGegenstandTestBad() {
+        int[] input1 = {15, 20, 10, 35};
+        int[] input2 = {20, 15, 40, 15};
+        int outputExpected = ElectronicShop.teurestGegenstand(input1, input2);
+        int output = 4;
+        assertFalse((output==outputExpected), "for test to succes output should be wrong");
+        System.out.println("\nTeurestGegenstand Test Bad Succes.");
 
     }
 
     @Test
-    public void unterBudget() {
+    public void unterBudgetTestGood() {
         int[] input1 = {15, 45, 20};
         int input2 = 30;
         int outputExpected = ElectronicShop.unterBudget(input1, input2);
         int output = 20;
         assertEquals(output, outputExpected);
-        System.out.println("\nUnterBudget Test Succes.");
+        System.out.println("\nUnterBudget Test Good Succes.");
+
+    }
+    @Test
+    public void unterBudgetTestBad() {
+        int[] input1 = {15, 45, 20};
+        int input2 = 30;
+        int outputExpected = ElectronicShop.unterBudget(input1, input2);
+        int output = 2;
+        assertFalse((output==outputExpected), "for test to succes output should be wrong");
+        System.out.println("\nUnterBudget Test Bad Succes.");
 
     }
 
     @Test
-    public void kannKaufen() {
+    public void kannKaufenTestGood() {
         int[] input1 = {40, 50, 60};
         int[] input2 = {8, 12};
         int input3 = 60;
         int outputExpected = ElectronicShop.kannKaufen(input1, input2, input3);
         int output = 58;
         assertEquals(output, outputExpected);
-        System.out.println("\nKannKaufen Test Succes.");
+        System.out.println("\nKannKaufen Test Good Succes.");
+
+    }
+    @Test
+    public void kannKaufenTestBad() {
+        int[] input1 = {40, 50, 60};
+        int[] input2 = {8, 12};
+        int input3 = 60;
+        int outputExpected = ElectronicShop.kannKaufen(input1, input2, input3);
+        int output = 5;
+        assertFalse((output==outputExpected), "for test to succes output should be wrong");
+        System.out.println("\nKannKaufen Test Bad Succes.");
 
     }
 }
--- src/Problem3Tests.java
+++ src/Problem3Tests.java
@@ -2,24 +2,36 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Problem3Tests {
     @Test
-    public void arrSumme() {
+    public void arrSummeTestGood() {
         int[] input1 = {1, 3, 0, 0, 0, 0, 0, 0, 0};
         int[] input2 = {8, 7, 0, 0, 0, 0, 0, 0, 0};
 
         int[] expectedOutput = Problem3.arrSumme(input1, input2);
         int[] output = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         assertArrayEquals(output, expectedOutput);
-        System.out.println("\nArrSumme Test Succes.");
+        System.out.println("\nArrSumme Test Good Succes.");
 
     }
 
     @Test
-    public void arrDiff() {
+    public void arrSummeTestBad() {
+        int[] input1 = {1, 3, 0, 0, 0, 0, 0, 0, 0};
+        int[] input2 = {8, 7, 0, 0, 0, 0, 0, 0, 0};
+
+        int[] expectedOutput = Problem3.arrSumme(input1, input2);
+        int[] output = {3, 0, 0, 0, 0, 0, 0, 0, 0};
+        assertFalse(Arrays.equals(expectedOutput, output), "for test to succes array should contain smth wrong");
+        System.out.println("\nArrSumme Test Bad Succes.");
+
+    }
+
+
+    @Test
+    public void arrDiffTestGood() {
         int[] input2 = {5, 4, 0, 0, 0, 0, 0, 0, 0};
         int[] input1 = {8, 3, 0, 0, 0, 0, 0, 0, 0};
 
@@ -27,32 +39,68 @@ public class Problem3Tests {
         int[] output = {2, 9, 0, 0, 0, 0, 0, 0, 0};
 //        System.out.println(Arrays.toString(output));
         assertArrayEquals(output, expectedOutput);
-        System.out.println("\nArrDiff Test Succes.");
+        System.out.println("\nArrDiff Test Good Succes.");
+
+    }
+    @Test
+    public void arrDiffTestBad() {
+        int[] input2 = {5, 4, 0, 0, 0, 0, 0, 0, 0};
+        int[] input1 = {8, 3, 0, 0, 0, 0, 0, 0, 0};
+
+        int[] expectedOutput = Problem3.arrDiff(input1, input2);
+        int[] output = {3, 9, 0, 0, 0, 0, 0, 0, 0};
+//        System.out.println(Arrays.toString(output));
+        assertFalse(Arrays.equals(expectedOutput, output), "for test to succes array should contain smth wrong");
+        System.out.println("\nArrDiff Test Bad Succes.");
 
     }
 
     @Test
-    public void arrMull() {
+    public void arrMulTestGood() {
         int[] input1 = {8, 3, 6, 0, 0, 0, 0, 0, 0};
         int input2 = 2;
 
         int[] expectedOutput = Problem3.arrMul(input1, input2);
         int[] output = {1, 6, 7, 2, 0, 0, 0, 0, 0, 0, 0};
         assertArrayEquals(output, expectedOutput);
-        System.out.println("\nArrMul Test Succes.");
+        System.out.println("\nArrMul Test Good Succes.");
+
+
+    }
+    @Test
+    public void arrMulTestBad() {
+        int[] input1 = {8, 3, 6, 0, 0, 0, 0, 0, 0};
+        int input2 = 2;
+
+        int[] expectedOutput = Problem3.arrMul(input1, input2);
+        int[] output = { 6, 7, 2, 0, 0, 0, 0, 0, 0, 0};
+        assertFalse(Arrays.equals(expectedOutput, output), "for test to succes array should contain smth wrong");
+        System.out.println("\nArrMul Test Bad Succes.");
 
 
     }
 
     @Test
-    public void arrDiv() {
+    public void arrDivTestGood() {
         int[] input1 = {2, 3, 6, 0, 0, 0, 0, 0, 0};
         int input2 = 2;
 
         int[] expectedOutput = Problem3.arrDiv(input1, input2);
         int[] output = {1, 1, 8, 0, 0, 0, 0, 0, 0};
         assertArrayEquals(output, expectedOutput);
-        System.out.println("\nArrDiv Test Succes.");
+        System.out.println("\nArrDiv Test Good Succes.");
+
+
+    }
+    @Test
+    public void arrDivTestBad() {
+        int[] input1 = {2, 3, 6, 0, 0, 0, 0, 0, 0};
+        int input2 = 2;
+
+        int[] expectedOutput = Problem3.arrDiv(input1, input2);
+        int[] output = {6,1, 8, 0, 0, 0, 0, 0, 0};
+        assertFalse(Arrays.equals(expectedOutput, output), "for test to succes array should contain smth wrong");
+        System.out.println("\nArrDiv Test Bad Succes.");
 
 
     }
