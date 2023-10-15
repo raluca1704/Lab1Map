fc68ba1a99545939f3c7b9ab2f207f6373fd3caf
raluc <raluca.chindris17@gmail.com>
2023-10-15 12:50:41 +0300

    am terminat testele la primele 3 pb alea bune

--- src/Problem3Tests.java
+++ src/Problem3Tests.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Problem3Tests {
@@ -17,10 +19,11 @@ public class Problem3Tests {
     @Test
     public void arrDiff(){
         int[] input2 = {5, 4, 0, 0, 0, 0, 0, 0, 0};
-        int[] input1 = {8, 7, 0, 0, 0, 0, 0, 0, 0};
+        int[] input1 = {8, 3, 0, 0, 0, 0, 0, 0, 0};
 
         int[] expectedOutput=Problem3.arrDiff(input1, input2);
-        int[] output={1, 6, 7, 2, 0, 0, 0, 0, 0, 0, 0};
+        int[] output={2,9,0,0,0,0,0,0,0};
+//        System.out.println(Arrays.toString(output));
         assertArrayEquals(output,expectedOutput);
         System.out.println("\nArrDiff Test Succes.");
 
@@ -31,7 +34,7 @@ public class Problem3Tests {
         int input2 = 2;
 
         int[] expectedOutput=Problem3.arrMul(input1, input2);
-        int[] output={2,9,0,0,0,0,0,0,0};
+        int[] output={1, 6, 7, 2, 0, 0, 0, 0, 0, 0, 0};
         assertArrayEquals(output,expectedOutput);
         System.out.println("\nArrMul Test Succes.");
 
@@ -39,6 +42,15 @@ public class Problem3Tests {
     }
     @Test
     public void arrDiv(){
+        int[] input1 = {2, 3, 6, 0, 0, 0, 0, 0, 0};
+        int input2 = 2;
+
+        int[] expectedOutput=Problem3.arrDiv(input1, input2);
+        int[] output={1, 1, 8, 0, 0, 0, 0, 0, 0};
+        assertArrayEquals(output,expectedOutput);
+        System.out.println("\nArrDiv Test Succes.");
+
+
 
     }
 
